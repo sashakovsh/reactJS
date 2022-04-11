d41535409dbb14142d4f364bbfe64c0201c6b429
alexander <sashackovsh@gmail.com>
2022-04-11 17:37:29 +0500

    lesson 2 homework

--- superapp/src/App.js
+++ superapp/src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
-import Message from './Message.js';
 import './Message.css';
+import Message from './Message';
+import React, { useEffect, useState } from 'react';
 
-function App(props) {
+function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [value, setValue] = useState(''); 
+
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  };
+  const handleSend = () => {
+    if (value !== '') {
+      const newMessage = {text: value, author: 'Alex'};
+      setMessageList([...messageList, newMessage]);
+      setValue(''); 
+    }
+  };
+  useEffect( () => {
+    let timer;
+    if(messageList.length > 0 
+      && messageList[messageList.length - 1].author !== 'bot') {
+        timer = setInterval( () => {
+          setMessageList([...messageList, newMessage])}, 2000);
+        const newMessage = {text: 'Я робот', author: 'bot'};
+      }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    }
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
-        <Message text={props.text} />
+        <h3>Чат с роботом</h3>
+        {messageList.map( (el, ind) => <Message text={el} key={ind}/>)}
+        <input 
+          placeholder={'Ваше сообщение'} 
+          onChange={handleInput} 
+          value={value} />
+        <button onClick={handleSend}>Отправить</button>
       </header>
     </div>
   );
--- superapp/src/index.js
+++ superapp/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const msgText = 'Hello, World';
-
 ReactDOM.render(
   <React.StrictMode>
-    <App text={msgText} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
